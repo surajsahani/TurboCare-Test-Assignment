42c060bd904d6590688bef9cd667a0273f5bd309
suraj <surajkumarsahani1997@gmail.com>
2021-05-25 22:23:07 +0530

    adapter

---
+++ app/src/main/java/martialcoder/surajsahani/turbocaretestassignment/MyAdapter.java
@@ -0,0 +1,37 @@
+//package martialcoder.surajsahani.turbocaretestassignment;
+//
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.BaseAdapter;
+//import android.widget.TextView;
+//
+//public class MyAdapter extends BaseAdapter {
+//
+//    // override other abstract methods here
+//
+//    @Override
+//    public int getCount() {
+//        return 0;
+//    }
+//
+//    @Override
+//    public Object getItem(int position) {
+//        return null;
+//    }
+//
+//    @Override
+//    public long getItemId(int position) {
+//        return 0;
+//    }
+//
+////    @Override
+////    public View getView(int position, View convertView, ViewGroup parent) {
+////        if (convertView == null) {
+////            convertView = getLayoutInflater().inflate(R.layout.list_item, container, false);
+////        }
+////
+////        ((TextView) convertView.findViewById(android.R.id.text1))
+////                .setText(getItem(position));
+////        return convertView;
+////    }
+//}
