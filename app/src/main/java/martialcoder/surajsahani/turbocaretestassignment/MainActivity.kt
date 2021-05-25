bc105e7ad8b7d0a61613431a5dcd467872a71de7
suraj <surajkumarsahani1997@gmail.com>
2021-05-25 13:16:16 +0530

    setting up intent

--- app/src/main/java/martialcoder/surajsahani/turbocaretestassignment/MainActivity.kt
+++ app/src/main/java/martialcoder/surajsahani/turbocaretestassignment/MainActivity.kt
@@ -1,11 +1,30 @@
-hubpackage martialcoder.surajsahani.turbocaretestassignment
+package martialcoder.surajsahani.turbocaretestassignment
+import android.content.Intent
+import androidx.appcompat.widget.Toolbar;
 
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar2)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.title = "Create new vehicle profile"
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        toolbar.navigationIcon!!.setTint(Color.BLACK)
+
+        val floatbutton = findViewById<FloatingActionButton>(R.id.floatingActionButton5)
+        floatbutton.setOnClickListener{
+            val intent = Intent(this, SelectMake::class.java)
+            startActivity(intent)
+        }
     }
 }
